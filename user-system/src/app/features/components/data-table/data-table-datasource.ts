fb32d7b90358f3bcabf4d52e3ac76c67f10c02fb
Nino Potskhoraia <nino.potskhoraia17@gmail.com>
2023-01-27 19:36:59 +0400

    populated data table with mock data

--- user-system/src/app/features/components/data-table/data-table-datasource.ts
+++ user-system/src/app/features/components/data-table/data-table-datasource.ts
@@ -6,32 +6,33 @@ import { Observable, of as observableOf, merge } from 'rxjs';
 
 // TODO: Replace this with your own data model type
 export interface DataTableItem {
-  name: string;
-  id: number;
+
+   email: string,
+   firstName: string,
+   lastName: string,
+   roles: string[],
+   status: string
 }
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: DataTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+{
+
+  email: 'email@gmail.com',
+  firstName: 'Nicole',
+  lastName: 'Davidson',
+  roles: ['user'],
+  status: 'active'
+},
+
+{
+
+  email: 'anne@gmail.com',
+  firstName: 'Abby',
+  lastName: 'Smith',
+  roles: ['admin'],
+  status: 'inactive'
+}
 ];
 
 /**
@@ -97,8 +98,10 @@ export class DataTableDataSource extends DataSource<DataTableItem> {
     return data.sort((a, b) => {
       const isAsc = this.sort?.direction === 'asc';
       switch (this.sort?.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
+        case 'email': return compare(a.email, b.email, isAsc);
+        case 'firstName':return compare(a.firstName, b.firstName, isAsc);
+        case 'lastName':return compare(a.lastName, b.lastName, isAsc);
+        case 'status':return compare(a.status, b.status, isAsc);
         default: return 0;
       }
     });
